99d429a6c51a80641bd3336d749dfe43f2b2c9ff
Yenni Permatasari <yennikristianip@gmail.com>
2020-03-15 23:28:16 +0700

    [Yenni] - Update slicing user login

--- src/components/users.js
+++ src/components/users.js
@@ -1,15 +1,81 @@
 import React from "react";
-import bangsam from "../assets/icons/user-page/bangsam_full.png";
+import { Form, Input, Button, Checkbox, Typography } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import Logo from "../assets/icons/logo-items/bangsam.png";
+import "../assets/styles/users.scss";
 
-const User = () => {
+const { Title, Text } = Typography;
+
+const Users = () => {
+  const onFinish = values => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
-    <div className="user">
-      <div className="user_left">
-        <img src={bangsam} alt="Bangsam"></img>
-      </div>
-      <div className="user_right"></div>
+    <div className="users">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true
+        }}
+        onFinish={onFinish}
+      >
+        <img src={Logo} alt="logo"></img>
+        <Title>WELCOME BACK!</Title>
+        <Text>Sign in to your account</Text>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!"
+            }
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!"
+            }
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
 
-export default User;
+export default Users;
