e8bf09e052c5f4d66f719fd19d6eb029a31aa84a
Yenni Permatasari <yennikristianip@gmail.com>
2020-03-16 01:21:54 +0700

    [Yenni] - New asset user page background

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import DashboardRoutes from "./routes/dashboardRoutes";
 import { Route } from "react-router-dom";
-import Users from "./components/users";
+import Users from "./components/users/signUp";
 
 const App = () => {
   return (
