974f147250755dda7c094d709577277cd2e647ef
Yenni Permatasari <yennikristianip@gmail.com>
2020-03-24 19:19:06 +0700

    [Yenni] - Update pick up details command description & destructure

--- src/components/dashboardDetails/pickupsDetails.js
+++ src/components/dashboardDetails/pickupsDetails.js
@@ -7,33 +7,64 @@ import moment from "moment";
 import "../../assets/styles/dashboard/dashboardDetails.scss";
 
 const PickupsDetails = () => {
+  //connect Redux to component
   const dispatch = useDispatch();
   const pickupDetails = useSelector(state => state.pickup.pickup);
   const param = useParams();
 
+  //use effect for dispatch data from redux
   useEffect(() => {
     dispatch(getAllPickup());
   }, [dispatch]);
 
+  //variable for filter data by id
   const pickupDetail = pickupDetails.filter(item => item._id === param.id)[0];
 
-  console.log("pickup", pickupDetail);
-
+  //condition if the main data is not loaded
   if (!pickupDetail) return <Spin size="large" tip="Loading..." />;
 
+  //variable for looping array data of waste details
   const wasteList = pickupDetail.item_details.map(type => ({
     waste: type.name,
     weight: type.weight
   }));
 
+  //variable for pick up address
   const pickupAddress = pickupDetail.address;
 
+  //variable for customer details
   const customerInfo = pickupDetail.user;
 
+  //variable for bank details
   const bankInfo = pickupDetail.branch;
+
+  //variable desructure data
+  const {
+    _id: idPickup,
+    estimated_time: estimatedTime,
+    amount,
+    createdAt,
+    updatedAt
+  } = pickupDetail;
+
+  const { village, regency, district, province } = pickupAddress;
+
+  const {
+    _id: idCustomer,
+    full_name: fullName,
+    phone_number: phoneNumber
+  } = customerInfo;
+
+  const {
+    _id: idBank,
+    branch_name: branchName,
+    phone_number: phoneNumberBank,
+    address
+  } = bankInfo;
 
   return (
     <div>
+      {/* Data display for general pick up info */}
       <Card className="pick-up">
         <Descriptions
           title="Pick Up Info"
@@ -41,19 +72,17 @@ const PickupsDetails = () => {
           size="small"
           bordered
         >
-          <Descriptions.Item label="Pick Up ID">
-            {pickupDetail._id}
-          </Descriptions.Item>
+          <Descriptions.Item label="Pick Up ID">{idPickup}</Descriptions.Item>
           <Descriptions.Item label="Estimated Time">
-            {pickupDetail.estimated_time}
+            {estimatedTime}
           </Descriptions.Item>
           <Descriptions.Item label="Amount">
             {" "}
             Rp
-            {pickupDetail.amount}
+            {amount}
           </Descriptions.Item>
           <Descriptions.Item label="Pick Up Address" span={3}>
-            {`${pickupAddress.village}, ${pickupAddress.regency}, ${pickupAddress.district}, ${pickupAddress.province}`}
+            {`${village}, ${regency}, ${district}, ${province}`}
           </Descriptions.Item>
           <Descriptions.Item label="Waste Detail" span={3}>
             <List
@@ -71,13 +100,14 @@ const PickupsDetails = () => {
             />
           </Descriptions.Item>
           <Descriptions.Item label="Created At">
-            {moment(pickupDetail.createdAt).format("MMMM Do YYYY, h:mm:ss a")}
+            {moment(createdAt).format("MMMM Do YYYY, h:mm:ss a")}
           </Descriptions.Item>
           <Descriptions.Item label="Updated At" span={2}>
-            {moment(pickupDetail.updatedAt).format("MMMM Do YYYY, h:mm:ss a")}
+            {moment(updatedAt).format("MMMM Do YYYY, h:mm:ss a")}
           </Descriptions.Item>
         </Descriptions>
       </Card>
+      {/* Data display for customer pick up info */}
       <Card className="pick-up">
         <Descriptions
           title="Customer Info"
@@ -86,28 +116,23 @@ const PickupsDetails = () => {
           bordered
         >
           <Descriptions.Item label="Customer ID">
-            {customerInfo._id}
+            {idCustomer}
           </Descriptions.Item>
-          <Descriptions.Item label="Name">
-            {customerInfo.full_name}
-          </Descriptions.Item>
+          <Descriptions.Item label="Name">{fullName}</Descriptions.Item>
           <Descriptions.Item label="Phone Number">
-            {customerInfo.phone_number}
+            {phoneNumber}
           </Descriptions.Item>
         </Descriptions>
       </Card>
+      {/* Data display for bank pick up info */}
       <Card className="pick-up">
         <Descriptions title="Bank Info" layout="vertical" size="small" bordered>
-          <Descriptions.Item label="Bank ID">{bankInfo._id}</Descriptions.Item>
-          <Descriptions.Item label="Name">
-            {bankInfo.branch_name}
-          </Descriptions.Item>
+          <Descriptions.Item label="Bank ID">{idBank}</Descriptions.Item>
+          <Descriptions.Item label="Name">{branchName}</Descriptions.Item>
           <Descriptions.Item label="Phone Number">
-            {bankInfo.phone_number}
+            {phoneNumberBank}
           </Descriptions.Item>
-          <Descriptions.Item label="Address">
-            {bankInfo.address}
-          </Descriptions.Item>
+          <Descriptions.Item label="Address">{address}</Descriptions.Item>
         </Descriptions>
       </Card>
     </div>
