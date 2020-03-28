8496371718bc80c5ea029009e50e4c7557720547
Yenni Permatasari <yennikristianip@gmail.com>
2020-03-29 05:59:12 +0700

    [Yenni] - Update landing page Bangsam team

--- src/components/LandingPage/contents/aboutBangsam.js
+++ src/components/LandingPage/contents/aboutBangsam.js
@@ -1,5 +1,8 @@
 import React from "react";
-import plant from "../../../assets/landingPage/Plant5.png";
+import dallah from "../../../assets/landingPage/teamBangsam/Dallah.png";
+import ricky from "../../../assets/landingPage/teamBangsam/Ricky.png";
+import jason from "../../../assets/landingPage/teamBangsam/Jason.png";
+import yenni from "../../../assets/landingPage/teamBangsam/Yenni.png";
 import "../../../assets/styles/LandingPage/aboutBangsam.scss";
 
 const AboutBangsam = () => {
@@ -24,23 +27,58 @@ const AboutBangsam = () => {
               further.
             </p>
             <p>
-              The purpose of this appilcation is to solve waste issues and to
-              improve the management of waste specifically household waste. With
-              the help of technology we appeal users to collect and separated
-              waste according to its kind to build a better environment. We
-              believe that this application could help users to participate to
-              create more efficient and productive way to manage waste in the
-              neighborhood as well as enhancing their income.
+              This application is a project continuation from Binar Academy to
+              Glints Academy batch 5. The purpose of this appilcation is to
+              solve waste issues and to improve the management of waste
+              specifically household waste. With the help of technology we
+              appeal users to collect and separated waste according to its kind
+              to build a better environment. We believe that this application
+              could help users to participate to create more efficient and
+              productive way to manage waste in the neighborhood as well as
+              enhancing their income.
             </p>
           </div>
-          <div className="about-bangsam__container__title">
-            <h1>Our Team</h1>
+        </div>
+        <div className="about-bangsam__team-container">
+          <h1 className="about-bangsam__team-container__title">Our Team</h1>
+          <div className="about-bangsam__team-container__cards-container">
+            <div className="about-bangsam__team-container__cards-container__card">
+              <div className="about-bangsam__team-container__cards-container__card__photo">
+                <img src={dallah} alt="back-end"></img>
+              </div>
+              <div className="about-bangsam__team-container__cards-container__card__text">
+                <h5>Afdallah W. A.</h5>
+                <p>Back-end Developer</p>
+              </div>
+            </div>
+            <div className="about-bangsam__team-container__cards-container__card">
+              <div className="about-bangsam__team-container__cards-container__card__photo">
+                <img src={ricky} alt="back-end"></img>
+              </div>
+              <div className="about-bangsam__team-container__cards-container__card__text">
+                <h5>Ricky</h5>
+                <p>Back-end Developer</p>
+              </div>
+            </div>
+            <div className="about-bangsam__team-container__cards-container__card">
+              <div className="about-bangsam__team-container__cards-container__card__photo">
+                <img src={yenni} alt="front-end"></img>
+              </div>
+              <div className="about-bangsam__team-container__cards-container__card__text">
+                <h5>Yenni K. P.</h5>
+                <p>Front-end Developer</p>
+              </div>
+            </div>
+            <div className="about-bangsam__team-container__cards-container__card">
+              <div className="about-bangsam__team-container__cards-container__card__photo">
+                <img src={jason} alt="mobile"></img>
+              </div>
+              <div className="about-bangsam__team-container__cards-container__card__text">
+                <h5>Jason A. L</h5>
+                <p>Mobile Developer</p>
+              </div>
+            </div>
           </div>
-          <img
-            className="about-bangsam__decoration"
-            src={plant}
-            alt="plant"
-          ></img>
         </div>
       </div>
     </div>
