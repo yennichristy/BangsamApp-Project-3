244bf209fd5abe868352fe7f9b2f736c802ef370
Yenni Permatasari <yennikristianip@gmail.com>
2020-03-30 21:11:59 +0700

    [Yenni] - Add animation

--- src/components/LandingPage/contents/homePage.js
+++ src/components/LandingPage/contents/homePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactWOW from "react-wow";
 import pageOne from "../../../assets/landingPage/homepage.svg";
 import top1 from "../../../assets/landingPage/top1.png";
 import top2 from "../../../assets/landingPage/top2.png";
@@ -23,19 +24,25 @@ const HomePage = () => {
           <img className="home-page__decoration4" src={bot2} alt="pics"></img>
         </div>
         <div className="home-page__left">
-          <div className="home-page__left__headline">
-            <p>Be a part of the SOLUTION not part of the POLLUTION</p>
-          </div>
-          <div className="home-page__left__button">
-            <button>Learn more</button>
-          </div>
+          <ReactWOW animation="animateUp">
+            <div className="home-page__left__headline">
+              <p>Be a part of the SOLUTION not part of the POLLUTION</p>
+            </div>
+          </ReactWOW>
+          <ReactWOW animation="animateUp" delay={"0.1s"}>
+            <div className="home-page__left__button">
+              <button>Learn more</button>
+            </div>
+          </ReactWOW>
         </div>
         <div className="home-page__right">
-          <img
-            className="home-page__right__picture"
-            src={pageOne}
-            alt="Bangsam"
-          ></img>
+          <ReactWOW animation="animateUp">
+            <img
+              className="home-page__right__picture"
+              src={pageOne}
+              alt="Bangsam"
+            ></img>
+          </ReactWOW>
         </div>
       </div>
     </div>
