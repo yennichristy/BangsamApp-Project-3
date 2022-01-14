cea182d30cb08d64170bbe5c122a29ec8277063c
Yenni <yennikristianip@gmail.com>
2022-01-14 13:23:49 +0700

    tweak: eslint disable

--- src/components/users/signIn.js
+++ src/components/users/signIn.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { Form, Input, Button, Checkbox, Typography } from "antd";
 import { LockOutlined, PhoneOutlined } from "@ant-design/icons";
